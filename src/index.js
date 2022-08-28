6cc7a88fcee5e39fe3c81ba1422d18b3db77efa3
jardejane <janefeitosalima49@gmail.com>
2022-08-28 05:07:22 -0300

    find: cryptography to model

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const connectDataBase= require('./database/database.js')
 
 const userRoute = require('./Users/users.route.js')
+const authRoute = require('./Auth/auth.routes.js')
 
 const port = process.env.PORT || 130 ;
 const app = express()
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json())
 
 app.use("/users", userRoute)
+app.use("auth", authRoute)
 
 app.listen(port, ()=> {
     console.log(`Server running at the door ${port}`)
